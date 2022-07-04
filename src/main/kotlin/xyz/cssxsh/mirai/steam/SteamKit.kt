7ec32b60efffcbefd20dc8cdc23a5c85bdcdda8b
cssxsh <cssxsh@gmail.com>
2022-07-05 01:38:23 +0800

    fix: avatar

--- src/main/kotlin/xyz/cssxsh/mirai/steam/SteamKit.kt
+++ src/main/kotlin/xyz/cssxsh/mirai/steam/SteamKit.kt
@@ -2,10 +2,17 @@ package xyz.cssxsh.mirai.steam
 
 import `in`.dragonbra.javasteam.enums.*
 import `in`.dragonbra.javasteam.handlers.*
+import `in`.dragonbra.javasteam.steam.handlers.steamfriends.*
 import `in`.dragonbra.javasteam.steam.steamclient.*
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.engine.okhttp.*
+import io.ktor.client.request.*
+import kotlinx.coroutines.sync.*
 import net.mamoe.mirai.contact.*
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.dnsoverhttps.DnsOverHttps
+import java.io.File
 
 public inline fun <reified H : ClientMsgHandler> SteamClient.handler(): H = getHandler(H::class.java)
 
@@ -21,5 +28,29 @@ public fun DnsOverHttps(url: String, ipv6: Boolean = false): DnsOverHttps {
 }
 
 public fun EResult.url(): String = "https://steamerrors.com/${code()}"
+
+private val http by lazy {
+    HttpClient(OkHttp)
+}
+
+private val mutex = Mutex()
+
+public suspend fun PersonaState.avatar(): File {
+    val hex = avatarHash.joinToString(separator = "") { byte -> "%02x".format(byte) }
+    val folder = File(System.getProperty("xyz.cssxsh.mirai.steam.avatar", "."))
+    val avatar = folder.resolve("${hex}_full.jpg")
+
+    mutex.withLock {
+        if (!avatar.exists()) {
+            folder.mkdirs()
+
+            val bytes: ByteArray = http.get("https://avatars.akamai.steamstatic.com/${hex}_full.jpg").body()
+
+            avatar.writeBytes(bytes)
+        }
+    }
+
+    return avatar
+}
 
 public val User.steam: SteamHelper by SteamHelper.Factory
